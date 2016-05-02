39c6deb320e060b71124ee0cc5b40d46b2b99c88
Nikhila Ravi <nikkiravi92@gmail.com>
2016-05-02 11:12:34 +0100

    Fixes addTrack mutation

--- index.js
+++ index.js
@@ -3,12 +3,14 @@ var graphql = require('graphql');
 var _ = require('lodash');
 var schema = require('./lib/schema');
 
-exports.handler = function (event, context) {
+exports.handler = function (event, context, callback) {
   console.log('Incoming Event', event);
   // In the introspection query from GraphiQL the variables key is not present in the event body
   var variables = event.variables && !_.isEmpty(event.variables) ? JSON.parse(event.variables) : {};
-  console.log('variables', variables);
   graphql.graphql(schema.root, event.query, null, variables)
-    .then(context.succeed)
+    .then((data) => {
+      console.log('RESOLVED DATA', data);
+      callback(null, data)
+    })
     .catch(context.fail);
 };
